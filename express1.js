5b99ab2670eef9d1f1ea1962da2d40c9954721af
Mandeep Saini <mandeep_saini@gmail.com>
2025-07-08 14:01:50 +0530

    first commit

---
+++ express1.js
@@ -0,0 +1,42 @@
+
+const express = require('express'); //Import
+
+
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+
+  res.send('Hello, Express!');
+
+});
+
+app.get('/aboutus', (req, res) => {
+
+  res.send('Hello, About Us!');
+  
+});
+
+app.get('/contactus', (req, res) => {
+
+  res.send('Hello, Contact Us!');
+  
+});
+
+app.get('/products', (req, res) => {
+
+  res.send('Hello, Products ');
+  
+});
+
+app.get('/product/:productname', (req, res) => {
+
+  res.send('Hello, Product ' +  req.params.productname );
+  
+});
+
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
